9641e6f902a746234c935a9b4db62f8b75017e28
azme-kabaha <auto.trans84@gmail.com>
2023-05-26 19:14:36 +0300

    note app with Flat list

--- App.tsx
+++ App.tsx
@@ -1,201 +1,105 @@
 
-// // my note app with scrollView///////
-//azme
+
+//------------------------my note app with   with flat list ----------------------------------
+
 
 import {
-  Button,
-  FlatList,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
-//import React from 'react'
-import {useState} from 'react';
-
-const App = () => {
-  // 1
-  const [value, setValue] = useState('');
-
-  const handeleOneChangeText = interdText => {
-    setValue(interdText);
-  };
-
-  // 2
-  const [listOfNotes, setlistOfNotes] = useState([]);
-
-  const handelOnPressButton = () => {
-    setlistOfNotes(currentNotes => [...currentNotes, value]);
-  };
-
-  return (
-    <View
-      style={{
-        padding: 60,
-		paddingHorizontal:15,
-		flex:1,
-      }}>
-
-		{/* to rander input along with button*/}
-
-      <View style={styles.inputContainer}>
-        <TextInput
-          onChangeText={handeleOneChangeText}
-          style={styles.inputStyle}
-          placeholder=" Add your Note Here"
-        />
-        <Button onPress={handelOnPressButton} color={'#000'} title="Add Note" />
-      </View>
-
-	  {/* to render all the notes that we created */}
-
-      <View style={styles.listContainer}>
-        <ScrollView>
-          {listOfNotes.map((item, index) => (
-            <Text style={styles.listitem} key={`item ${index}`}>
-              {item}
-            </Text>
-          ))}
-        </ScrollView>
-      </View>
-
-    </View>
-  );
-};
-
-export default App;
-
-const styles = StyleSheet.create({
-  inputContainer: {
-	flex:1,
-    flexDirection: 'row',
-    paddingBottom: 30,
-    borderBottomWidth: 1,
-	alignItems:'center',
-	justifyContent:'space-between',
-  },
-  inputStyle: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: '#ddd',
-  },
-  listContainer: {
-    paddingTop: 30,
-	flex:3,
-  },
-  listitem: {
-    borderRadius: 1,
-    borderColor: 'red',
-    backgroundColor: 'green',
-    padding: 20,
-    marginBottom: 20,
-    color: '#fff',
-    fontSize: 20,
-  },
-});
-
-//------------------------ the same app but with flat list ----------------------------------
-
-
-// // my note app with flatList///////
-
-// import {
-// 	Button,
-// 	FlatList,
-// 	ScrollView,
-// 	StyleSheet,
-// 	Text,
-// 	TextInput,
-// 	View,
-//   } from 'react-native';
-//   //import React from 'react'
-//   import {useState} from 'react';
+	Button,
+	FlatList,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TextInput,
+	View,
+  } from 'react-native';
+  //import React from 'react'
+  import {useState} from 'react';
   
-//   const App = () => {
-// 	// 1
-// 	const [value, setValue] = useState('');
+  const App = () => {
+	// 1
+	const [value, setValue] = useState('');
   
-// 	const handeleOneChangeText = interdText => {
-// 	  setValue(interdText);
-// 	};
+	const handeleOneChangeText = interdText => {
+	  setValue(interdText);
+	};
   
-	// 2
-// 	const [listOfNotes, setlistOfNotes] = useState([]);
+	2
+	const [listOfNotes, setlistOfNotes] = useState([]);
   
-// 	const handelOnPressButton = () => {
-// 	  setlistOfNotes(currentNotes => [...currentNotes, value]);
-// 	};
+	const handelOnPressButton = () => {
+	  setlistOfNotes(currentNotes => [...currentNotes, value]);
+	};
   
-// 	return (
-// 	  <View
-// 		style={{
-// 		  padding: 60,
-// 		  paddingHorizontal:15,
-// 		  flex:1,
-// 		}}>
+	return (
+	  <View
+		style={{
+		  padding: 60,
+		  paddingHorizontal:15,
+		  flex:1,
+		}}>
   
-// 		  {/* to rander input along with button*/}
+		  {/* to rander input along with button*/}
   
-// 		<View style={styles.inputContainer}>
-// 		  <TextInput
-// 			onChangeText={handeleOneChangeText}
-// 			style={styles.inputStyle}
-// 			placeholder=" Add your Note Here"
-// 		  />
-// 		  <Button onPress={handelOnPressButton} color={'#000'} title="Add Note" />
-// 		</View>
+		<View style={styles.inputContainer}>
+		  <TextInput
+			onChangeText={handeleOneChangeText}
+			style={styles.inputStyle}
+			placeholder=" Add your Note Here"
+		  />
+		  <Button onPress={handelOnPressButton} color={'#000'} title="Add Note" />
+		</View>
   
-// 		{/* to render all the notes that we created */}
+		{/* to render all the notes that we created */}
   
-// 		<View style={styles.listContainer}>
+		<View style={styles.listContainer}>
 
-// 		  <FlatList
-// 		  data={listOfNotes}
-// 		  renderItem={(itemData)=>(
-// 			<Text  style={styles.listitem}>{itemData.item}</Text>
-// 		  )}
-// 		 />
+		  <FlatList
+		  data={listOfNotes}
+		  renderItem={(itemData)=>(
+			<Text  style={styles.listitem}>{itemData.item}</Text>
+		  )}
+		 />
 			
-// 		</View>
+		</View>
   
-// 	  </View>
-// 	);
-//   };
+	  </View>
+	);
+  };
   
-//   export default App;
+  export default App;
   
-//   const styles = StyleSheet.create({
-// 	inputContainer: {
-// 	  flex:1,
-// 	  flexDirection: 'row',
-// 	  paddingBottom: 30,
-// 	  borderBottomWidth: 1,
-// 	  alignItems:'center',
-// 	  justifyContent:'space-between',
-// 	},
-// 	inputStyle: {
-// 	  flex: 1,
-// 	  borderWidth: 1,
-// 	  borderColor: '#ddd',
-// 	},
-// 	listContainer: {
-// 	  paddingTop: 30,
-// 	  flex:3,
-// 	},
-// 	listitem: {
-// 	  borderRadius: 1,
-// 	  borderColor: 'red',
-// 	  backgroundColor: 'green',
-// 	  padding: 20,
-// 	  marginBottom: 20,
-// 	  color: '#fff',
-// 	  fontSize: 20,
-// 	},
-//   });
+  const styles = StyleSheet.create({
+	inputContainer: {
+	  flex:1,
+	  flexDirection: 'row',
+	  paddingBottom: 30,
+	  borderBottomWidth: 1,
+	  alignItems:'center',
+	  justifyContent:'space-between',
+	},
+	inputStyle: {
+	  flex: 1,
+	  borderWidth: 1,
+	  borderColor: '#ddd',
+	},
+	listContainer: {
+	  paddingTop: 30,
+	  flex:3,
+	},
+	listitem: {
+	  borderRadius: 1,
+	  borderColor: 'red',
+	  backgroundColor: 'green',
+	  padding: 20,
+	  marginBottom: 20,
+	  color: '#fff',
+	  fontSize: 20,
+	},
+  });
 
   //--------------------------------------------------------------------
-  
+            // convert react To react native
+
 // import { Button, StyleSheet, Text, View } from 'react-native'
 // import {useState} from 'react';
 
